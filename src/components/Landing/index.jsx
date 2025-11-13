31bc48c2b03dab28161b7d4d3ea9284b6894f8af
Raffa <203495392+RaffaHitipeuw@users.noreply.github.com>
2025-11-13 11:29:18 +0700

    First Commit
    Responsive Splash & Hero Section

---
+++ src/components/Landing/index.jsx
@@ -0,0 +1,34 @@
+"use client";
+import { motion } from "framer-motion";
+import styles from "./style.module.scss";
+import { slideUp } from "./animation";
+import dynamic from "next/dynamic";
+
+// Ganti import ini:
+const ColorBends = dynamic(() => import("./ColorBends"), { ssr: false });
+
+export default function LandingPage() {
+  return (
+    <motion.main
+      variants={slideUp}
+      initial="initial"
+      animate="enter"
+      className={styles.landing}
+    >
+      <div className="w-full h-screen relative overflow-hidden">
+        <ColorBends
+          colors={["#DF2935"]}
+          rotation={0}
+          speed={0.2}
+          scale={1}
+          frequency={1}
+          warpStrength={1}
+          mouseInfluence={1}
+          parallax={0.5}
+          noise={0.01}
+          transparent={true}
+        />
+      </div>
+    </motion.main>
+  );
+}
